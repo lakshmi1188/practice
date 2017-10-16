542d0abbdb64b5159a261cf0692e0a0f8ae038c7
user <user@DESKTOP-U59MBCV>
2017-10-16 16:12:22 +0530

    commit

---
+++ src/main/java/com/selenium/practice/util/Helperclass.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.selenium.practice.util;
+
+/**
+ * @author user
+ *
+ */
+public class Helperclass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.print("c");
+	}
+
+}
